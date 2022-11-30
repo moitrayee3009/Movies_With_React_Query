4c0f209a6fc6ed3aeff391ec067edac4306a3cec
Moitrayee Choudhury <moitrayee3009@gmail.com>
2022-11-30 10:32:19 +0100

    created components folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const List = React.lazy(() => import('./components/List/List'))
+const Details = React.lazy(() => import('./components/Details/Details'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/List' componentt={List} />
+            <Route exact path='/Details' component={Details} />
+            <Route exact path='/' component={List} />
+          </Switch>
+        </BrowserRouter>
+      </Suspense>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// import './App.css'
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <header className='App-header'></header>
+//     </div>
+//   )
+// }
+
+// export default App
